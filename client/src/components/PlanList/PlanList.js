a0daa1fa83bd31fd24fc0e6a1733d5aa75fffafd
Logan Wood <lwood3499@gmail.com>
2019-04-25 08:18:29 -0500

    comments for PlanList.js

--- client/src/components/PlanList/PlanList.js
+++ client/src/components/PlanList/PlanList.js
@@ -40,24 +40,34 @@ const PlanList = () => {
                 setShowModal(false);
                 setCollectCompares([]);
         };
+        //  creates default array to store plans that are sorted or filtered
         const [sortedPlans, setSortedPlans] = useState([]);
+        // boolean that flags whether to show different lists in render
         const [isSorted, setIsSorted] = useState(false);
+        // accesses name attribute from event to process sort request
         const handleSort = e => {
+                // create array copy because sort mutates array in place
                 let newPlans = [...plans];
+                // sorts by name of plan
                 if (e.target.id === 'byName') {
                         newPlans.sort((a, b) => {
+                                // if unicode value of a.name > b.name then return positive for sorting
                                 if (a.name > b.name) {
                                         return 1;
                                 }
+                                // unicode value comparison for sorting
                                 if (a.name < b.name) {
                                         return -1;
                                 }
+                                // plans have same value so no change
                                 return 0;
                         });
                         setIsSorted(true);
                         setSortedPlans(newPlans);
                 } else if (e.target.id === 'byPrice') {
+                        // sort by price
                         newPlans.sort((a, b) => {
+                                // sort by price value
                                 if (a.price > b.price) {
                                         return 1;
                                 }
@@ -70,21 +80,27 @@ const PlanList = () => {
                         setSortedPlans(newPlans);
                 }
         };
+        // used to filter data array
         const handleFilter = e => {
                 let newPlans = [];
                 if (e.target.id === 'byBestSellers') {
+                        // only passes objects with bestSellers
                         newPlans = plans.filter(plan => plan.bestSellers);
                 } else if (e.target.id === 'byComprehensive') {
+                        // only passes objects with comprehensive type
                         newPlans = plans.filter(plan => plan.type === 'Comprehensive');
                 } else if (e.target.id === 'byFixed') {
+                        // only passes objects with fixed type
                         newPlans = plans.filter(plan => plan.type === 'Fixed');
                 }
                 setIsSorted(true);
                 setSortedPlans(newPlans);
         };
+        // longer list of sorting options, so select component was created
         const [section, setSection] = useState('');
         const handleSection = e => {
                 let newPlans = [];
+                // use swtich statement to determine which filter to use
                 switch (e.target.value) {
                         case 'Travel Medical':
                                 newPlans = plans.filter(plan => plan.section === 'Travel Medical');
@@ -117,7 +133,10 @@ const PlanList = () => {
                 setShowList(!showList);
         };
 
+        // creates array that will collect up to 4 plans to compare in the modal
         const [collectCompares, setCollectCompares] = useState([]);
+        // needs to confirm that there are at least 2 plans and no more than 4
+        // also flags if user tries to pick the same plan twice to compare
         const handleComparison = id => {
                 let flag = false;
                 collectCompares.forEach(collection => {
@@ -136,12 +155,19 @@ const PlanList = () => {
                 }
         };
 
+        // displays all plans in default order
         const plansDisplay = plans.map(plan => (
+                // uses Plan component to add styling
                 <Plan {...plan} key={plan.id} handleComparison={handleComparison} />
         ));
+
+        // passes any plan list that has been sorted or filtered
         const sortedPlansDisplay = sortedPlans.map(plan => (
+                // uses Plan component to add styling
                 <Plan {...plan} key={plan.id} handleComparison={handleComparison} />
         ));
+
+        // creates a list for the modal comparison if the collectCompares array has more than 1 plan
         const comparePlansDisplay =
                 collectCompares.length > 0
                         ? collectCompares.map(plan => (
